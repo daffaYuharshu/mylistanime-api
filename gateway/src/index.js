71d2e571818dd285c96e17cb1ae56edd84c5da7c
daffaYuharshu <daffayuharshu@gmail.com>
2024-06-17 18:19:45 +0700

    add hello world

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -7,12 +7,25 @@ const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(cors());
+app.get("/", (req, res) => {
+    res.send(`Hello World!`);
+})
+
 // http://localhost:3000/ => http://localhost:3001/ USER
 app.use("/user", createProxyMiddleware({
     target: process.env.USER_SERVICE,
     pathRewrite: {
         '^/user': ''
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying request: ${req.method} ${req.url} -> ${process.env.USER_SERVICE}`);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        console.log(`Received response from ${process.env.USER_SERVICE}: ${proxyRes.statusCode}`);
+    },
+    onError: (err, req, res) => {
+        console.error(`Error proxying request: ${err.message}`);
+        res.status(500).send('Proxy error');
     }
 }));
 
@@ -21,6 +34,16 @@ app.use("/animes", createProxyMiddleware({
     target: process.env.ANIME_SERVICE,
     pathRewrite: {
         '^/animes': ''
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying request: ${req.method} ${req.url} -> ${process.env.ANIME_SERVICE}`);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        console.log(`Received response from ${process.env.ANIME_SERVICE}: ${proxyRes.statusCode}`);
+    },
+    onError: (err, req, res) => {
+        console.error(`Error proxying request: ${err.message}`);
+        res.status(500).send('Proxy error');
     }
 }));
 
