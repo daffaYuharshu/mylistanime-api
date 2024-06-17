0f243c5854dd7814189334f40da970c21c634220
daffaYuharshu <daffayuharshu@gmail.com>
2024-06-17 18:30:21 +0700

    change route

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -7,45 +7,21 @@ const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send(`Hello World!`);
-})
+// // http://localhost:3000/ => http://localhost:3001/ USER
+// app.use("/user", createProxyMiddleware({
+//     target: process.env.USER_SERVICE,
+//     pathRewrite: {
+//         '^/user': ''
+//     }
+// }));
 
-// http://localhost:3000/ => http://localhost:3001/ USER
-app.use("/user", createProxyMiddleware({
-    target: process.env.USER_SERVICE,
-    pathRewrite: {
-        '^/user': ''
-    },
-    onProxyReq: (proxyReq, req, res) => {
-        console.log(`Proxying request: ${req.method} ${req.url} -> ${process.env.USER_SERVICE}`);
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        console.log(`Received response from ${process.env.USER_SERVICE}: ${proxyRes.statusCode}`);
-    },
-    onError: (err, req, res) => {
-        console.error(`Error proxying request: ${err.message}`);
-        res.status(500).send('Proxy error');
-    }
-}));
-
-// http://localhost:3000/animes => http://localhost:3002/ ANIME
-app.use("/animes", createProxyMiddleware({
-    target: process.env.ANIME_SERVICE,
-    pathRewrite: {
-        '^/animes': ''
-    },
-    onProxyReq: (proxyReq, req, res) => {
-        console.log(`Proxying request: ${req.method} ${req.url} -> ${process.env.ANIME_SERVICE}`);
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        console.log(`Received response from ${process.env.ANIME_SERVICE}: ${proxyRes.statusCode}`);
-    },
-    onError: (err, req, res) => {
-        console.error(`Error proxying request: ${err.message}`);
-        res.status(500).send('Proxy error');
-    }
-}));
+// // http://localhost:3000/animes => http://localhost:3002/ ANIME
+// app.use("/animes", createProxyMiddleware({
+//     target: process.env.ANIME_SERVICE,
+//     pathRewrite: {
+//         '^/animes': ''
+//     }
+// }));
 
 app.listen(port, () => {
     console.log(`API Gateway service listening on port ${port}`);
