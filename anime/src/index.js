bb6157fe4b0fa5dc5d016f346b14201596ca0663
daffaYuharshu <daffayuharshu@gmail.com>
2024-06-17 18:30:34 +0700

    change route

--- anime/src/index.js
+++ anime/src/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use("/", routerAnime);
+app.use("/animes", routerAnime);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
