dc2622b8eefc51fd2d1a713955b27914236d1ae9
daffaYuharshu <daffayuharshu@gmail.com>
2024-06-17 17:51:12 +0700

    change proxy config

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -4,10 +4,13 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.use(cors());
 // http://localhost:3000/ => http://localhost:3001/ USER
 app.use("/user", createProxyMiddleware({
-    target: `http://localhost:3001`,
+    target: process.env.USER_SERVICE,
     pathRewrite: {
         '^/user': ''
     }
@@ -15,7 +18,7 @@ app.use("/user", createProxyMiddleware({
 
 // http://localhost:3000/animes => http://localhost:3002/ ANIME
 app.use("/animes", createProxyMiddleware({
-    target: `http://localhost:3002`,
+    target: process.env.ANIME_SERVICE,
     pathRewrite: {
         '^/animes': ''
     }
